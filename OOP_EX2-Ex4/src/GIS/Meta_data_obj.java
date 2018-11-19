817c42c2021767854fa1123678e46ae3aedd13ef
Liad Cohen <liad.code+git@gmail.com>
2018-11-19 21:43:58 +0200

    Added Meta_data_obj class, added fields for: Color, Date . Added constructor

---
+++ OOP_EX2-Ex4/src/GIS/Meta_data_obj.java
@@ -0,0 +1,30 @@
+package GIS;
+
+import Geom.Point3D;
+
+import java.awt.*;
+
+public class Meta_data_obj implements Meta_data {
+    Color color;
+    long UTCtime;
+
+    public Meta_data_obj(Color rgbColor, long UTCtimeLONG){
+        this.color = rgbColor;
+        this.UTCtime = UTCtimeLONG;
+    }
+    /**
+     * returns the Universal Time Clock associated with this data;
+     */
+    @Override
+    public long getUTC() {
+        return 0;
+    }
+
+    /**
+     * @return the orientation: yaw, pitch and roll associated with this data;
+     */
+    @Override
+    public Point3D get_Orientation() {
+        return null;
+    }
+}
