a3e154f8b997651db1ce5c95bc9ec6aae7c059e7
Timor Sharabi <xposionn@gmail.com>
2018-11-26 17:52:11 +0200

    Finish

--- OOP_Ex2-Ex4/src/File_format/Main.java
+++ OOP_Ex2-Ex4/src/File_format/Main.java
@@ -7,5 +7,6 @@ public class Main {
     public static void main(String[] args) {
         Csv2kml csv2kml = new Csv2kml();
         csv2kml.makeKmlFile("WigleWifi_20171201110209.csv");
+        //test
     }
 }
