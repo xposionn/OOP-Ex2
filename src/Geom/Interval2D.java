927b54b1af87983359e59e8535ca80d89589e2a2
Liad Cohen <liad.code+git@gmail.com>
2018-11-27 21:12:18 +0200

    Added Interval2D fields, getters, setters, constructor. javadoc incl.

--- src/Geom/Interval2D.java
+++ src/Geom/Interval2D.java
@@ -1,4 +1,34 @@
 package Geom;
 
+/**
+ * This class represents an Interval between two points on a 2D surface.
+ *
+ * @author Liad and Timor.
+ */
 public class Interval2D {
+
+    private Point3D firstP, secondP;
+
+
+    public Interval2D(Point3D firstP, Point3D secondP) {
+        this.firstP = firstP;
+        this.secondP = secondP;
+    }
+
+
+
+    public void setFirstP(Point3D firstP) {
+        this.firstP = firstP;
+    }
+
+    public void setSecondP(Point3D secondP) {
+        this.secondP = secondP;
+    }
+    public Point3D getFirstP() {
+        return firstP;
+    }
+
+    public Point3D getSecondP() {
+        return secondP;
+    }
 }
