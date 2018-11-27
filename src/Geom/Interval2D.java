347049d27caab0abbba12fbfe0d17aa78651e48e
Liad Cohen <liad.code+git@gmail.com>
2018-11-27 21:40:49 +0200

    Interval2D added 2d/3d distance function (not yet implemented)

--- src/Geom/Interval2D.java
+++ src/Geom/Interval2D.java
@@ -5,7 +5,7 @@ package Geom;
  *
  * @author Liad and Timor.
  */
-public class Interval2D {
+public class Interval2D implements Geom_element{
 
     private Point3D firstP, secondP;
 
@@ -30,5 +30,25 @@ public class Interval2D {
 
     public Point3D getSecondP() {
         return secondP;
+    }
+
+    /**
+     * 3D distance from a 2d Interval to a 3d point doesn't make sense. we will calculate the 2d distance anyway.
+     * @param p another Point to calculate distance to.
+     * @return the shortest distance between any point in our Interval to another point given.
+     */
+    @Override
+    public double distance3D(Point3D p) {
+        return distance2D(p);
+    }
+
+    /**
+     * Calculates the shortest distance available between our Interval and a point given. could be calculated from any point in our Interval.
+     * @param p another Point to calculate distance to.
+     * @return  the shortest distance between (any point in our) Interval to another point given.
+     */
+    @Override
+    public double distance2D(Point3D p) {
+        return 0; //not yet implemented
     }
 }
