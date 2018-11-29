786e22eb47b209121db8bd9f3e5d05a04de4a474
Liad Cohen <liad.code+git@gmail.com>
2018-11-30 01:18:38 +0200

    Timestamp and Date are separated as in KML example for assignment

--- src/GIS/Meta_data_obj.java
+++ src/GIS/Meta_data_obj.java
@@ -2,6 +2,8 @@ package GIS;
 
 import Geom.Point3D;
 
+import java.util.ArrayList;
+
 
 public class Meta_data_obj implements Meta_data {
     String name; //name of the obj (placemark or layer)
@@ -9,7 +11,7 @@ public class Meta_data_obj implements Meta_data {
     long UTCtime; //timestamp in UTC (long) of the recording time of this placemark (or creation time of a layer).
     String type; //type will be used to indicate whether specific placemark is a Player (P) or Food/Fruit (F)
     Double speed; //speed is the speed in meters/sec of a player.
-
+    ArrayList<String> wifiPointEx2DATA; //all data used in Ex2 to be saved for same-KML as requested.
 
     Double radius; //radius is the 'eating radius' of a player, the smallest distance a player can eat a fruit.
 
@@ -17,7 +19,16 @@ public class Meta_data_obj implements Meta_data {
         this.name = name;
         this.UTCtime = UTCtimeLONG;
     }
-    public Meta_data_obj(String name, long UTCtimeLONG, String type, double speed, double radius){ //constructor used relevant for Ex3
+
+//    //a constructor used relevant for Ex2
+//    public Meta_data_obj(String name, long UTCtimeLONG){ //constructor used relevant for Ex2, we only need name and time.
+//        this.name = name;
+//        this.UTCtime = UTCtimeLONG;
+//        this.wifiPointEx2DATA.
+//    }
+
+    //a constructor used relevant for Ex3
+    public Meta_data_obj(String name, long UTCtimeLONG, String type, double speed, double radius){
         this.name = name;
         this.UTCtime = UTCtimeLONG;
         this.type = type;
@@ -28,15 +39,15 @@ public class Meta_data_obj implements Meta_data {
     public String allInfo(){ //name is also handled separately, as a placemark name.
         StringBuilder info = new StringBuilder();
         info.append("Name: " + this.name +"\n");
-        info.append("Time (UTC): " + this.UTCtime + " = " + Algorithms.TimeChange.longtoUTC(this.UTCtime) +"\n");
+        info.append("Timestamp (UTC): " + this.UTCtime +"\n");
+        info.append("Date: " + Algorithms.TimeChange.longtoUTC(this.UTCtime).replaceAll("T"," ").replaceAll("Z"," "));
         if(this.color!=null)
             info.append("Color in HEX: " + this.color +"\n");
-        if(this.type != null) {
+        if(this.type != null)
             if (this.type.equals("P"))
                 info.append("Type: Player\n");
-            if (this.type.equals("F"))
+            else if (this.type.equals("F"))
                 info.append("Type: Fruit\n");
-        }
         if (this.speed != null)
             info.append("Speed: " + this.speed +"\n");
         if(this.radius != null)
