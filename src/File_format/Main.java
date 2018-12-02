7011df2429aaf1eb8de275f6c220ef22d1ec8975
Timor Sharabi <xposionn@gmail.com>
2018-12-02 22:04:50 +0200

    Added tests for MyCoords. needed to fix Azimut test.

--- src/File_format/Main.java
+++ src/File_format/Main.java
@@ -3,12 +3,10 @@ package File_format;
 
 import Algorithms.MultiCSV;
 
-import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         MultiCSV.createProject("dataExamples");
         Csv2kml.makeKmlFile("WigleWifi_20171201110209.csv");
-
     }
 }
