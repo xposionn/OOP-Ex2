6a30b4f7c9c744fd32f7fa42d082519fc3c10d64
Timor Sharabi <xposionn@gmail.com>
2018-12-02 17:13:46 +0200

    created 3 Planes for test, testa testb testc

--- Tests/Coords/MyCoordsTest.java
+++ Tests/Coords/MyCoordsTest.java
@@ -7,27 +7,59 @@ import static org.junit.jupiter.api.Assertions.*;
 class MyCoordsTest {
 
     MyCoords coords = new MyCoords();
-    Point3D b9 = new Point3D(32.103315, 35.209039, 670);
-    Point3D bHummus = new Point3D(32.106352, 35.205225, 650);
-    Point3D vectorBetween = new Point3D(337.6989921, -359.2492069, -20);
+
+    Point3D testa1 = new Point3D(32.103315, 35.209039, 670);
+    Point3D testa2 = new Point3D(32.106352, 35.205225, 650);
+    Point3D vtesta = new Point3D(337.6989921, -359.2492069, -20);
+
+//    Point3D testb1 = new Point3D();
+//    Point3D testb2 = new Point3D();
+//    Point3D vtestb = new Point3D();
+//
+//
+//    Point3D testc1 = new Point3D();
+//    Point3D testc2 = new Point3D();
+//    Point3D vtestc = new Point3D();
+
+
+
+
     Point3D zeroPt = new Point3D(0, 0, 0);
     Point3D onLonoPt = new Point3D(-10, 0, 0);
     final double ERROR_MARGIN = 0.0001;
 
     @Test
-    void add() {
-        coords.add(b9, vectorBetween);
+    void adda() {
+        Point3D aPlusvTesta = coords.add(testa1, vtesta);
+        assertEquals(aPlusvTesta.x(), testa2.x(),ERROR_MARGIN);
+        assertEquals(aPlusvTesta.y(), testa2.y(),ERROR_MARGIN);
+        assertEquals(aPlusvTesta.z(), testa2.z(),ERROR_MARGIN);
     }
+//    @Test
+//    void addb() {
+//        Point3D bPlusvTestb = coords.add(testb1, vtestb);
+//        assertEquals(bPlusvTestb.x(), testb2.x(),ERROR_MARGIN);
+//        assertEquals(bPlusvTestb.y(), testb2.y(),ERROR_MARGIN);
+//        assertEquals(bPlusvTestb.z(), testb2.z(),ERROR_MARGIN);
+//    }
+
+//    @Test
+//    void addc() {
+//        Point3D cPlusvTestc = coords.add(testc1, vtestc);
+//        assertEquals(cPlusvTestc.x(), testc2.x(),ERROR_MARGIN);
+//        assertEquals(cPlusvTestc.y(), testc2.y(),ERROR_MARGIN);
+//        assertEquals(cPlusvTestc.z(), testc2.z(),ERROR_MARGIN);
+//    }
 
     @Test
     void distance3d() {
-        assertEquals(493.45780156501763,coords.distance3d(b9, bHummus),ERROR_MARGIN); //from excel.
+        assertEquals(493.45780156501763,coords.distance3d(testa1, testa2),ERROR_MARGIN); //from excel.
     }
 
     @Test
     void vector3D() {
-        Point3D diffVector = coords.vector3D(b9, bHummus);
-        Point3D expected = vectorBetween; //for easier readability
+        Point3D diffVector = coords.vector3D(testa1, testa2);
+        Point3D expected = vtesta; //for easier readability
         assertEquals(expected.x(), diffVector.x(), ERROR_MARGIN,"Something is wrong with the X logic in Vector3D function");
         assertEquals(expected.y(), diffVector.y(), ERROR_MARGIN,"Something is wrong with the Y logic in Vector3D function");
         assertEquals(expected.z(), diffVector.z(), ERROR_MARGIN,"Something is wrong with the Z logic in Vector3D function");
@@ -43,15 +75,15 @@ class MyCoordsTest {
 
     @Test
     void isValid_GPS_Point() {
-        boolean test1 = coords.isValid_GPS_Point(vectorBetween);
+        boolean test1 = coords.isValid_GPS_Point(vtesta);
         if(test1){
-            fail(vectorBetween.toString() + "is not valid.");
+            fail(vtesta.toString() + "is not valid.");
         }
-        boolean test2 = coords.isValid_GPS_Point(b9);
+        boolean test2 = coords.isValid_GPS_Point(testa1);
         if(test2) {
         //all good
         }else{
-            fail(b9 + " is vaild.");
+            fail(testa1 + " is vaild.");
         }
 
     }
--- src/File_format/Main.java
+++ src/File_format/Main.java
@@ -3,9 +3,12 @@ package File_format;
 
 import Algorithms.MultiCSV;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         MultiCSV.createProject("dataExamples");
         Csv2kml.makeKmlFile("WigleWifi_20171201110209.csv");
+
     }
 }
