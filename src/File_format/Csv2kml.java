42026f7a2d53121003a31d53b2108b70646ae13a
Liad Cohen <liad.code+git@gmail.com>
2018-12-02 17:01:57 +0200

    Complete javadoc for Csv2kml

--- src/File_format/Csv2kml.java
+++ src/File_format/Csv2kml.java
@@ -6,16 +6,16 @@ import java.io.File;
 import java.io.IOException;
 
 /**
- * This class will transform a CSV table (as in arrayList) to a KML file to work with Google Earth.
+ * This class will transform a CSV file to a CsvTable (as in arrayList) to GIS_layer and then to KML file to work with Google Earth.
  */
 public class Csv2kml {
 
     /**
-     * This method will transform a CSV table (as in arrayList) to a GIS_layer, and then to KML file to work with Google Earth.
-     * @param fileName
+     * This method will transform a CSV file into CsvTable (as in arrayList) to a GIS_layer, and then to KML file to work with Google Earth.
+     * @param fileName fileName of the CSV file to transform into KML file.
      */
     public static void makeKmlFile(String fileName) {
-        String path = System.getProperty("user.dir")+ File.separator;;
+        String path = System.getProperty("user.dir")+ File.separator;
         File csv = new File(path+File.separator+"dataExamples"+File.separator+fileName);
         CsvTable csvTable = null;
         try {
