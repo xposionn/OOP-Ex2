21d841e61e239448aef8367e8901ee5218da8159
Liad Cohen <liad.code+git@gmail.com>
2018-12-07 15:25:05 +0200

    Added required classes, designed basic GUI MyFrame

---
+++ src/Game/MyFrame.java
@@ -0,0 +1,11 @@
+package Game;
+
+import javax.swing.*;
+
+public class MyFrame {
+    private JLabel welcomeHeader;
+    private JLabel developedByFooter;
+    private JButton loadGameBtn;
+    private JButton saveGameBtn;
+    private JButton newGameBtn;
+}
