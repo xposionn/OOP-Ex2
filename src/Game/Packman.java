61e65ef71a5ac3b1bbdac8b52d2e1118c57e49a7
Liad Cohen <liad.code+git@gmail.com>
2018-12-07 19:15:26 +0200

    Added Packman

--- src/Game/Packman.java
+++ src/Game/Packman.java
@@ -1,5 +1,24 @@
 package Game;
 
-public class Packman {
+import Geom.Point3D;
 
+public class Packman {
+    private Point3D location;
+    private long speed;
+    private long orientationHORIZONTAL;
+    private long orientationVERTICAL;
+
+    /**
+     * Constructor for out packman robot.
+     * @param location point3d location in lat,lon,alt.
+     * @param speed the packman speed.
+     * @param orientationHORIZONTAL its horizontal orientation.
+     * @param orientationVERTICAL its vertical orientation.
+     */
+    public Packman(Point3D location, long speed, long orientationHORIZONTAL, long orientationVERTICAL) {
+        this.location = location;
+        this.speed = speed;
+        this.orientationHORIZONTAL = orientationHORIZONTAL;
+        this.orientationVERTICAL = orientationVERTICAL;
+    }
 }
