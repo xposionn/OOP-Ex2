d84a46bfddff794bf9cc318db4b79d84d875c5b5
Timor Sharabi <xposionn@gmail.com>
2018-12-18 01:02:38 +0200

    Created Solution2KML

---
+++ src/File_format/Solution2KML.java
@@ -0,0 +1,22 @@
+package File_format;
+
+import Algorithms.Solution;
+
+import java.io.File;
+
+public class Solution2KML {
+    Solution solution;
+
+    public Solution2KML(Solution solution) {
+        this.solution = solution;
+    }
+
+    public void toKML(){
+        double finishtime = solution.timeToComplete();
+        //Create an empty file
+        ClassLoader classLoader = getClass().getClassLoader();
+        File kml = new File(String.format("%s/gametoKML.kml", ClassLoader.getResource(".").getFile()));
+
+    }
+
+}
